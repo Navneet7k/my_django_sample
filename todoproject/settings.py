ba4ba10051b1642ad0199cd2928f534f049aa0a9
Apple <apple@Apples-Air.domain.name>
2022-10-21 22:42:49 +0530

    settings host fixes

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4qe4*3pg_78^%o&2w3a91b*=%uf=pviah6^noonx7xra)wrq72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lottery-scrapper.herokuapp.com']
 
 # Application definition
 
